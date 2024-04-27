0be17ee4209e5241eb28dcff0df2129621020068
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2024-04-27 18:05:24 +0530

    select querys

---
+++ statement.sql
@@ -0,0 +1,12 @@
+select * from Parks_and_Recreation.employee_salary;
+
+select distinct age,
+ gender 
+ from Parks_and_Recreation.employee_demographics;
+ 
+ select distinctrow gender
+ from employee_demographics;
+ 
+ select age , 
+ age -10
+ from employee_demographics;
